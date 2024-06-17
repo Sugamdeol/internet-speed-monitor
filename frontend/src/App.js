cc1955cac58596187840c117ccfe9460dd8dfe44
Sugam Deol <92591383+Sugamdeol@users.noreply.github.com>
2024-06-17 15:32:49 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SpeedMonitor from './SpeedMonitor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Internet Speed Monitor</h1>
+      <SpeedMonitor />
+    </div>
+  );
+}
+
+export default App;
